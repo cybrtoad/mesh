de0ebedafba7af127fc61d40d8fd4ac9500605b9
CybrToad <34751856+cybrtoad@users.noreply.github.com>
2022-08-30 08:56:53 -0600

    Initial commit

---
+++ src/py/base.py
@@ -0,0 +1,16 @@
+# The Mesh
+# by Tamara Linse
+# Tagline: Your Family's Online Ecosystem
+# Purpose: A more prosocial internet
+
+# @author:       Tamara Linse
+# @file:         base.py
+# @language:     python
+# @initialDate:  8.30.2022
+# @lastUpdate:   8.30.2022
+
+## BEGIN
+
+print("Hello World!")
+
+## END
